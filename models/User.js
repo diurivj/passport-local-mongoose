d571832bf7cd7864b1ce7ecdda7aa7b411953955
diurivj <diurivj@icloud.com>
2019-09-09 11:19:44 -0500

    first commit

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+const PLM = require('passport-local-mongoose');
+
+const userSchema = new Schema(
+  {
+    email: String,
+    name: String,
+    lastname: String
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+userSchema.plugin(PLM, { usernameField: 'email' });
+
+module.exports = model('User', userSchema);
---
+++ routes/auth.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const passport = require('../config/passport');
+
+router.get('/signup', (req, res, next) => {
+  const config = {
+    title: 'Sign Up',
+    action: '/signup',
+    button: 'Sign up',
+    register: true // esto lo pongo para decirle al form que es registro y muestre el name y el lastname
+  };
+  res.render('auth/form', config);
+});
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.register({ ...req.body }, req.body.password);
+    console.log(user);
+    res.redirect('/login');
+  } catch (e) {
+    console.log(e);
+    res.send('El usuario ya existe');
+  }
+});
+
+router.get('/login', (req, res, next) => {
+  const config = {
+    title: 'Log in',
+    action: '/login',
+    button: 'Login'
+  };
+  res.render('auth/form', config);
+});
+
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  console.log(req.user, req.session);
+  res.redirect('/profile');
+});
+
+router.get('/profile', isLoggedIn, (req, res, next) => {
+  res.render('auth/profile', { user: req.user });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+module.exports = router;
